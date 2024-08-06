f83eb80ee5321198eda002422912dadcdd116ed2
jaozc <jgabrielzcosta@gmail.com>
2024-08-06 18:47:36 -0300

    Added CRUD func to Pizza Controller

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -29,8 +29,40 @@ public class PizzaController : ControllerBase
     }
 
     // POST action
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {            
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+    }
 
     // PUT action
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        if (id != pizza.Id)
+            return BadRequest();
+            
+        var existingPizza = PizzaService.Get(id);
+        if(existingPizza is null)
+            return NotFound();
+    
+        PizzaService.Update(pizza);           
+    
+        return NoContent();
+    }
 
     // DELETE action
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var pizza = PizzaService.Get(id);
+    
+        if (pizza is null)
+            return NotFound();
+        
+        PizzaService.Delete(id);
+    
+        return NoContent();
+    }
 }
